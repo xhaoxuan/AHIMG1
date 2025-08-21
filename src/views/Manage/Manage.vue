a6a610b3a1a5a4153a898bd2636b069c3b29d84f
xhaoxuan <864488607@qq.com>
2025-08-21 22:27:25 +0800

    Update Manage.vue

--- src/views/Manage/Manage.vue
+++ src/views/Manage/Manage.vue
@@ -2,7 +2,7 @@
   <section class="Manage pt-4 sm:pt-6">
     <div class="pt-6 flex items-center text-sm">
       <div class="ml-4">
-        <Input v-model="secretKey" placeholder="请输入密钥" />
+        <input v-model="secretKey" placeholder="请输入密钥" class="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50" />
       </div>
       <div class="ml-4">
         <Button @click="getImages">查询</Button>
@@ -19,7 +19,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import Input from '@/components/ui/input/Input.vue';
+
 import { Button } from '@/components/ui/button';
 
 const secretKey = ref('');
